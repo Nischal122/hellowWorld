81f160d6e1660862fa3e54ec8fd7f6afb4f2c104
Nischal122 <technicalnazwa@gmail.com>
2023-02-21 12:12:29 +0800

    calculator that can do +-*/ commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = float(input("write first number "))
+b = float(input("write second number "))
+c = str(input("what do u want to do;add(+),subtract(-),multiply(*),divide(/) "))
+
+if c == "+":
+    add = (a+b)
+    print("Sum between two numbers are;",round(add, 3))
+elif c == "-":
+    add = (a-b)
+    print("difference between two numbers are;",round(add, 3))
+elif c == "*":
+    add = (a*b)
+    print("multiplication between two numbers are;", round(add, 3))
+elif c == "/":
+    add = (a/b)
+    print("division between two numbers are;", round(add, 3))
+else:
+    print("get lost :( its a error")
